414491ecc6b468afe629f9d5b202fb0ea2adaa27
Akmal Rafique <akmalrafique123456@gmail.com>
2026-05-12 02:52:42 +0800

    add task 1

---
+++ SECJ1023-Programming-Technique-II/Midterm 19_20 practical.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+class Student{
+    private:
+    string name;
+    string matric;
+    double gpa;
+    double cgpa;
+    int cred;
+    int ccred;
+    string conf;
+    public:
+
+
+
+};
+
+
+
+
+
+using namespace std;
+int main(){
+
+}
