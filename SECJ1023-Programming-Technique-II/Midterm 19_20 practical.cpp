4af31ba2508e28de3d8c2c0b106114694c8158f2
Akmal Rafique <akmalrafique123456@gmail.com>
2026-05-12 04:05:17 +0800

    add task

--- SECJ1023-Programming-Technique-II/Midterm 19_20 practical.cpp
+++ SECJ1023-Programming-Technique-II/Midterm 19_20 practical.cpp
@@ -10,15 +10,56 @@ class Student{
     int ccred;
     string conf;
     public:
-
-
-
+    Student( string n = "", string mn= "",double CGPA = 0,int c = 0){
+        name =n;
+        matric=mn;
+        cgpa=CGPA;
+        ccred=c;
+        gpa=0.0;
+        cred=0;
+        updateCGPA();
+    }
+    string getName()const{
+        return name;
+        }
+    string getStatus()const{
+        if(cgpa<1.7){
+            return "Fail Status";
+        }
+        if(cgpa>1.7 && cgpa <2.0){
+            return "Probation Status";
+        }
+        else
+        return "Good Status";
+    }
+    void updateCGPA(double cg){
+        cgpa=((gpa*cred)+(cg*ccred))/(cred+ccred);
+    }
+    void readInput(){
+        cout<<"Name:";
+        cin>>name;
+        cout<<"Matric Number:";
+        cin>>matric;
+        cout<<"CGPA:";
+        cin>>cgpa;
+        cout<<"Total Credit:";
+        cin>>ccred;
+        cout<<"GPA:";
+        cin>>gpa;
+        cout<<"Credit earned this semester:";
+        cin>>cred;
+        updateCGPA(cgpa);
+    }
+    bool operator==(const Student& right)const{
+        return(this->getStatus()==right.getStatus());
+    }
+    friend void dispInfo();
 };
 
-
-
-
-
+void dispInfo(){
+    cout<<"Hello, "<<p1.name<<endl;
+    cout<<"You are in year "<<    <<"in "<<
+}
 using namespace std;
 int main(){
 
