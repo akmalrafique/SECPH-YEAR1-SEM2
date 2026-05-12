ce3e7a247cfdec06f40286237e2278366c972c76
Akmal Rafique <akmalrafique123456@gmail.com>
2026-05-12 15:08:53 +0800

    add overload operaator

--- SECJ1023-Programming-Technique-II/Midterm 21_22.cpp
+++ SECJ1023-Programming-Technique-II/Midterm 21_22.cpp
@@ -11,10 +11,33 @@ class Line{
         c=C;
     }
     void read();
-
-
+    Line operator-(const Line& right)const;
+    bool operator!=(const Line& right)const;
 };
 void Line:: read(){
     cout<<"Enter the slope (m) and y-intercept of a line (c) =>";
     cin>>m>>c;
+}
+Line Line::operator-(const Line& right)const{
+    Line line3;
+    line3.m=this->m - right.m;
+    line3.c=this->c - right.c;
+    return line3;
+}
+bool Line:: operator!=(const Line& right)const{
+    if(this->m * right.m ==-1){
+        return true;
+    }
+        else{
+            return false;
+        }
+}
+int main(){
+    Line lines[3];
+
+
+
+
+
+    return 0;
 }
