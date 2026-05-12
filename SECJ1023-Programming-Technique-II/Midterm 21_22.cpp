6f838e02fa52b2a5474c107fc1e2ebd598b48fa7
Akmal Rafique <akmalrafique123456@gmail.com>
2026-05-12 14:43:46 +0800

    add task 1

---
+++ SECJ1023-Programming-Technique-II/Midterm 21_22.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class Line{
+    private:
+    int m;
+    int c;
+    public:
+    Line(int M=0,int C =0){
+        m=M;
+        c=C;
+    }
+
+
+};
