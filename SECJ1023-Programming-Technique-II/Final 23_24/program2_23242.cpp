a3571156eab4898b0f8da75e070e2cbf4eb6362a
Akmal Rafique <akmalrafique123456@gmail.com>
2026-07-08 14:15:20 +0800

    add files

---
+++ SECJ1023-Programming-Technique-II/Final 23_24/program2_23242.cpp
@@ -0,0 +1,7 @@
+// Programming Technique II (SECSJ1023)
+// Semester 2, 2023/2024
+
+// Final Exam (Practical - Question 2)
+
+// Student's Name:.....................
+// Matric Number:.....................
