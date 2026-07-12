5e694117a5ad32982b898d626dedfd001b9d327f
Akmal Rafique <akmalrafique123456@gmail.com>
2026-07-12 21:10:59 +0800

    add files

---
+++ SECJ1023-Programming-Technique-II/Final 24_25/program1_24252.cpp
@@ -0,0 +1,118 @@
+// NAME: 
+// MATRIC NO.:
+// SECTION:
+
+// SECJ1023 PROGRAMMING TECHNIQUE 2
+// FINAL EXAM (PRACTICAL) 2024/2025-02
+// QUESTION 1 (a-f)
+
+/*
+(a)(i)	Developer and Employee: 
+Relationship: 
+
+(a)(ii)	Manager and Team:
+Relationship: 
+
+(a)(iii) Company and Employee: 
+Relationship: 
+
+(a)(iv)	Team and Employee: 
+Relationship: 
+*/
+
+#include <iostream>
+using namespace std;
+
+// Base class - Partially given
+class Employee {
+public:
+    // (b)(i) virtual void work() const
+
+    // (b)(ii) virtual ~Employee()
+
+};
+
+// Derived classes - Fully given
+class Developer : public Employee {
+public:
+    void work() const {
+        cout << "Developer writing code\n";
+    }
+};
+
+// Derived classes - Fully given
+class Manager : public Employee {
+public:
+    void work() const {
+        cout << "Manager in meeting\n";
+    }
+};
+
+// Company owns its employees (1:1 relationship) - Partially given
+class Company {
+private:
+    Employee* employee;  
+public:
+    // (c)(i) Company(Employee *)
+
+    // (c)(ii) ~Company()
+    
+    // (c)(iii) void dailyOperation() const
+
+};
+
+// Team uses employees - Partially given
+class Team {
+private:
+    Employee* members[3];  // Fixed-size array
+    int count = 0;
+public:
+    // (d)(i) void addMember(Employee *)
+
+    // (d)(ii) void teamMeeting() const
+
+};
+
+// main() - Partially given
+int main() {
+
+    // Relationship 1
+    Employee* employees[2];
+    employees[0] = new Developer();
+    employees[1] = new Manager();
+
+    for (int i = 0; i < 2; i++) {
+        // (e)(i) invokes the work() function for each Employee in the array
+
+    }
+
+    // Relationship 2
+    // (e)(ii) create Company object and pass the dynamically allocated Developer object
+
+    // (e)(ii) invokes the dailyOperation() function
+
+
+    // Relationship 3
+    Team projectTeam;
+    // (e)(iii) invoke the addMember() function and pass the dynamically allocated Developer object 
+
+    // (e)(iii) invoke the addMember() function and pass the dynamically allocated Manager object
+
+    // (e)(iii) invoke the teamMeeting() function
+    
+
+    // destroy objects
+    for (int i = 0; i < 2; i++) {
+        delete employees[i];
+    }
+
+	system("pause"); //! remove this line if you are using DevC++
+    return 0;
+}
+
+// (f) Ensure your program produces exactly the following output, without any compilation or runtime errors:
+//			Developer writing code
+//			Manager in meeting
+//			Company operation: Developer writing code
+//			Developer writing code
+//			Manager in meeting
